e027b8081a6b48e07d967f6feaccfcb1e853218f
ros1992 <samiromerocardenas@gmail.com>
2025-10-09 02:59:50 +0000

    actualizacion de autenticacion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\\Http\\Controllers;
+namespace App\Http\Controllers;
 
-use Illuminate\\Http\\Request;
-use Illuminate\\Support\\Facades\\Auth;
-use App\\Models\\User;
-use Illuminate\\Support\\Facades\\Hash;
-use Tymon\\JWTAuth\\Facades\\JWTAuth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -19,12 +19,12 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            \'email\' => \'required|string|email\',
-            \'password\' => \'required|string\',
+            'email' => 'required|string|email',
+            'password' => 'required|string',
         ]);
 
         if (! $token = JWTAuth::attempt($credentials)) {
-            return response()->json([\'message\' => \'Unauthorized\'], 401);
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         return $this->respondWithToken($token);
@@ -39,26 +39,27 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validatedData = $request->validate([
-            \'name\' => \'required|string|max:255\',
-            \'email\' => \'required|string|email|max:255|unique:users\',
-            \'password\' => \'required|string|min:6\',
-            \'role\' => \'required|string|in:administrator,supervisor\'
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'role' => 'required|string|in:administrator,supervisor'
         ]);
 
         $user = User::create([
-            \'name\' => $validatedData[\'name\'],
-            \'email\' => $validatedData[\'email\'],
-            \'password\' => Hash::make($validatedData[\'password\']),\n            \'role\' => $validatedData[\'role\'],
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => $validatedData['role'],
         ]);
 
         $token = JWTAuth::fromUser($user);
 
         return response()->json([
-            \'message\' => \'User created successfully\',
-            \'user\' => $user,
-            \'authorisation\' => [
-                \'token\' => $token,
-                \'type\' => \'bearer\',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
             ]
         ]);
     }
@@ -71,7 +72,7 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
-        return response()->json([\'message\' => \'Successfully logged out\']);
+        return response()->json(['message' => 'Successfully logged out']);
     }
 
     /**
@@ -94,10 +95,10 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            \'access_token\' => $token,
-            \'token_type\' => \'bearer\',
-            \'user\' => Auth::user(),
-            \'expires_in\' => JWTAuth::factory()->getTTL() * 60
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => Auth::user(),
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
         ]);
     }
 }
